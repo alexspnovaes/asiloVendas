6667b214a8a448e5f05f0840ea666d6b14a500cd
alexandreamcom <alexandre.novaes@amcom.com.br>
2019-09-02 19:52:32 -0300

    a

--- AiloVendas/AiloVendas/Form1.Designer.cs
+++ AiloVendas/AiloVendas/Form1.Designer.cs
@@ -28,38 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // dataGridView1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 100);
-            this.tableLayoutPanel1.TabIndex = 0;
-            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.TableLayoutPanel1_Paint);
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.Padding = new System.Windows.Forms.Padding(10);
+            this.dataGridView1.RowTemplate.Height = 240;
+            this.dataGridView1.Size = new System.Drawing.Size(788, 438);
+            this.dataGridView1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "PDV";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- AiloVendas/AiloVendas/Form1.cs
+++ AiloVendas/AiloVendas/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,9 +17,53 @@ namespace AiloVendas
         public Form1()
         {
             InitializeComponent();
+            fillData();
         }
 
         private void TableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void fillData()
+        {
+            //first column
+            DataGridViewImageColumn imgCol = new DataGridViewImageColumn();
+            imgCol.HeaderText = "Photo";
+            imgCol.Name = "Photo";
+            dataGridView1.Columns.Add(imgCol);
+
+            //second
+            DataGridViewImageColumn imgCol1 = new DataGridViewImageColumn();
+            imgCol1.HeaderText = "Album";
+            dataGridView1.Columns.Add(imgCol1);
+
+            //third
+            DataGridViewImageColumn imgCol2 = new DataGridViewImageColumn();
+            dataGridView1.Columns.Add(imgCol2);
+
+            //data
+            //first row
+            WebClient wc = new WebClient();
+            byte[] bytes = wc.DownloadData("https://loremflickr.com/320/240");
+            MemoryStream ms = new MemoryStream(bytes);
+
+            Image img = System.Drawing.Image.FromStream(ms);
+            //Image img1 = Image.FromFile("@caminho");
+            //Image img2= Image.FromFile("@caminho");
+            Object[] row = new Object[] { img };
+            dataGridView1.Rows.Add(row);
+
+            ////secd row
+            //img = Image.FromFile("@caminho");
+            //img1 = Image.FromFile("@caminho");
+            //img2 = Image.FromFile("@caminho");
+            //row = new Object[] { img, img1, img2 };
+            //dataGridView1.Rows.Add(row);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
